fb9fb42433be816c2bd2e161b5daf3a1abb8ac37
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-03-01 12:16:30 -0600

    Se implementa useAuthStore

--- src/components/PaginaLogin.tsx
+++ src/components/PaginaLogin.tsx
@@ -1,12 +1,42 @@
+import { useEffect } from "react";
 import { useAuthStore } from "../store/auth.store"
 
 export const PaginaLogin = () => {
     const authStatus = useAuthStore(state => state.status);
+    const usuario = useAuthStore(state => state.usuario);
+    const login = useAuthStore(state => state.login);
+    const logout = useAuthStore(state => state.logout);
+
+    useEffect(() => {
+        setTimeout(() => {
+            logout();
+        }, 1500);
+    },[]);
     
+    if (authStatus === 'checando') {
+        return <h3>Cargando...</h3>
+    }
+
     return (
         <>
             <h3>Página de Login</h3>
-            {authStatus}
+            {
+                (authStatus === 'autenticado'
+                ? <div>Autenficado como: {JSON.stringify(usuario,null,2)}</div>
+                : <div>No Autenticado</div>)
+            }
+
+            {
+                (authStatus === 'autenticado'
+                ? (
+                    <button onClick={logout}>Salir</button>
+                )
+                : (
+                    <button onClick={()=>login('zutjmx@gmail.com','sistemas')}>
+                        Entrar
+                    </button>
+                ))
+            }
         </>
     )
 }
--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -1,16 +1,37 @@
 import { create } from "zustand";
 
 interface Authstate {
-    status: 'autenteticado' | 'noautenticado' | 'checando';
+    status: 'autenticado' | 'noautenticado' | 'checando';
     token?: string;
     usuario?: {
         nombre: string;
         email: string;
-    }
+    };
+    login: (email: string, password: string) => void;
+    logout: () => void;
 }
 
 export const useAuthStore = create<Authstate>()((set) => ({
     status: 'checando',
     token: undefined,
     usuario: undefined,
+
+    login: (email: string, password: string) => {
+        set({
+            status: 'autenticado',
+            token: 'cch-86256108',
+            usuario: {
+                nombre: 'Jesús Zúñiga Trejo',
+                email: email,
+            }
+        });
+    },
+
+    logout: () => {
+        set({
+            status: 'noautenticado',
+            token: undefined,
+            usuario: undefined
+        });
+    },
 }));
